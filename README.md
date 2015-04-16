d06f0f0e68774e29c8186f5ad69d1b56f9dd599f
German Gonzalez <german.gonzalez@solem.cl>
2015-04-15 23:36:24 -0300

    Adding OpenXava generated App Folder

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # recruitment-app
 Openxava's based Recruitment App, Lifecycle of candidate
+
+Current Openxava version: 5.2.1
---
+++ src/org/recruitment/domain/Candidate.java
@@ -0,0 +1,40 @@
+package org.recruitment.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Candidate {
+
+	@Id
+	private String id;
+	
+	private String firstName;
+	
+	private String lastName;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	
+}
